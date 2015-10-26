ee1a92ff0b707a368ea750c0bba27592c9bd1ceb
OlegMasnik <olegmasnik@gmail.com>
2015-10-26 12:32:42 +0200

    First commit

---
+++ android/src/com/badlogicgames/superjumper/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package com.badlogicgames.superjumper.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.badlogicgames.superjumper.SuperJumper;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useCompass = false;
+		config.useAccelerometer = false;
+		initialize(new SuperJumper(), config);
+	}
+}
